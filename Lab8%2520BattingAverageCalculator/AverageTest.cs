6863e3d9b09f63c6473cbd64e2fdb8e8ba076d82
Mo Yang <moyang9192@gmail.com>
2016-09-16 23:40:29 -0400

    Add files via upload

---
+++ Lab8%2520BattingAverageCalculator/AverageTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab8_BattingAverageCalculator;
+
+namespace Lab8_BattingTest
+{
+    [TestClass]
+    public class AverageTest
+    {
+        [TestMethod]
+        public void TestBattingPercentageIs0WhenNoRuns()
+        {
+            int[] battingSheet = new int[5];
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 0;
+            battingSheet[3] = 0;
+            battingSheet[4] = 0;
+
+            double battingAverage = Program.CalculateBattingAverage(battingSheet); //passing in battingSheet (array of integers), get back a double and name it. work from right to left. Right now returning 0.5, because defined inside method as 0.5.
+            Assert.AreEqual(0, battingAverage);
+
+            //average should be equal to 0 (expected), battingAverage=actual calculated by the method 
+        }
+
+        [TestMethod]
+        public void TestBattingPercentageIs60()
+        {
+            int[] battingSheet = new int[5];
+
+            ///write in actual program:
+            //  for (int index = 0; index < battingSheet.Length; index++)
+            // {
+
+            //          battingSheet[index] = 0;
+            // }
+
+
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 2;
+            battingSheet[3] = 4;
+            battingSheet[4] = 1;
+
+
+            double battingAverage = Program.CalculateBattingAverage(battingSheet); //passing in battingSheet (array of integers), get back a double and name it. work from right to left. Right now returning 0.5, because defined inside method as 0.5.
+            Assert.AreEqual(0.6, battingAverage);
+        }
+
+        [TestMethod]
+        public void TestSluggingPercentage()
+        {
+            int[] battingSheet = new int[5];
+            battingSheet[0] = 3;
+            battingSheet[1] = 2;
+            battingSheet[2] = 1;
+            battingSheet[3] = 0;
+            battingSheet[4] = 4;
+
+            double sluggingAverage = Program.CalculateSluggingAverage(battingSheet);
+            Assert.AreEqual(2, sluggingAverage);
+
+        }
+    }
+}
+
+
+
+/*
+
+ Test1: Calculate Batting Percentage
+ when you pass the method, don't have to include the type again. take and return should be same type. 
+ */
---
+++ Lab8%2520BattingAverageCalculator/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_BattingAverageCalculator
+{
+    public class Program
+    {
+
+        static string keepGoing;
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Batting Average Calculator!");
+            do
+            {
+              
+                Console.WriteLine("\nEnter number of times at bat: ");
+
+                int batNum = int.Parse(Console.ReadLine())+1;
+
+                int[] battingSheet = new int[batNum];
+    
+                //int iCount=0;
+                for (int iCount = 1; iCount < batNum; iCount++)
+                {     
+                  
+                    Console.Write("Result for at-bat "+iCount +": ");
+                    battingSheet[iCount] = int.Parse(Console.ReadLine());
+                }
+
+                /*Console.Write("Result for at-bat 2: ");
+                battingSheet[1] = int.Parse(Console.ReadLine());
+                Console.Write("Result for at-bat 3: ");
+                battingSheet[2] = int.Parse(Console.ReadLine());
+                Console.Write("Result for at-bat 4: ");
+                battingSheet[3] = int.Parse(Console.ReadLine());
+                Console.Write("Result for at-bat 5: \n");
+                battingSheet[4] = int.Parse(Console.ReadLine());
+                */
+
+                //double Average = CalculateBattingAverage(battingSheet);
+                //Console.WriteLine(Average);
+
+                Console.WriteLine("\nBatting average: " + CalculateBattingAverage(battingSheet));
+                Console.WriteLine("Slugging average: " + CalculateSluggingAverage(battingSheet) + "\n");
+
+                continueOrQuit();
+                
+            } while (keepGoing.ToLower() == "y" || keepGoing.ToLower() == "yes");
+
+        }
+
+        public static double CalculateBattingAverage(int[] battingSheet)
+        {
+            int hit = 0;
+
+            for (int i = 0; i < battingSheet.Length; i++)
+            {
+          
+               // battingSheet[index] = 0;
+
+                if (battingSheet[i] > 0)
+                {
+                    hit = hit + 1;
+                }
+            }
+                       
+             double batAvg = (double) hit / battingSheet.Length;
+             return Math.Round(batAvg, 3);
+            //return 0.0; //to pass the 1st test
+            //return 0.5;
+
+        }
+
+        public static double CalculateSluggingAverage(int[] battingSheet)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < battingSheet.Length; i++)
+            {
+                sum += battingSheet[i];
+            }
+
+            double sluggingAvg = (double) sum / battingSheet.Length;
+            return Math.Round(sluggingAvg, 3);
+
+        }
+
+        private static void continueOrQuit()
+        {
+            Console.WriteLine("Another batter? (y/n)");
+            keepGoing = Console.ReadLine();
+        }
+
+
+    }
+}
+/*
+
+            int successAtBat = 0;
+            for (int i = 0; i < battingSheet.Length;i++)
+            { if (battingSheet[1] > 0)
+                   successAtBat++;
+             }
+            return (double)successAtBat / battingSheet.Length;
+
+ * 3/5=0
+Return numberator/denominator
+Return (double) numberofHits/numberOfAttempts
+
+
+    for (int i = 0; i < batting.Length; i++)
+            {
+                expNetStudents[i] = new Student();
+            }
+
+ This method calculates batting average, give it array of integers, that returns double
+ methods go inside class
+
+Enter number of times at bat:
+2 3 4 4 4 
+
+1 0 3 4 2 
+
+    batting sheet array
+    array of integers
+    Index = At Bat 0,1,2,3,4
+    Value = Runs Earned 
+ */
